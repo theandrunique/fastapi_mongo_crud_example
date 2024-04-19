f0d8919cb71a7dea5e99d120d5db77c1522fa2f9
theandrunique <theandrunique@gmail.com>
2024-04-19 16:02:17 +0300

    Add config.py

---
+++ src/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=True,
+    )
+
+    PROJECT_NAME: str = "Authorization Server"
+    SERVER_HOST: str = "http://localhost"
+
+    MONGO_URL: str
+    MONGO_DATABASE_NAME: str
+
+
+settings = Settings()  # type: ignore
