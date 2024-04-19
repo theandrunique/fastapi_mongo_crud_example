6271879875fb8fd852754e49c2d0b0a7309ffde5
theandrunique <theandrunique@gmail.com>
2024-04-19 20:01:32 +0300

    Add validators for ObjectId for pydantic

--- src/mongo.py
+++ src/mongo.py
@@ -1,8 +1,11 @@
 import os
-from typing import Annotated
+from typing import Any
+
+import bson
 import motor.motor_asyncio
-from bson import ObjectId as _ObjectId
-from pydantic import AfterValidator
+from pydantic import GetCoreSchemaHandler, GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import CoreSchema, core_schema
 from pymongo.errors import ConnectionFailure
 
 from src.config import settings
@@ -35,10 +38,25 @@ async def test_connection() -> None:
         os._exit(1)
 
 
-def check_object_id(value: str) -> str:
-    if not _ObjectId.is_valid(value):
-        raise ValueError('Invalid ObjectId')
-    return value
+class PyObjectId(bson.ObjectId):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: GetCoreSchemaHandler,
+    ) -> CoreSchema:
+        def validate(value: str) -> PyObjectId:
+            if not PyObjectId.is_valid(value):
+                raise ValueError("Invalid ObjectId")
+            return PyObjectId(value)
 
+        return core_schema.no_info_plain_validator_function(
+            function=validate,
+            serialization=core_schema.to_string_ser_schema(),
+        )
 
-ObjectId = Annotated[str, AfterValidator(check_object_id)]
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
