27bfdfc9ff78798229254cdddf8c88d6efdc49af
theandrunique <theandrunique@gmail.com>
2024-04-25 23:34:01 +0300

    Update tests to work with new logic of connection to MongoDB

--- tests/test_items/conftest.py
+++ tests/test_items/conftest.py
@@ -1,14 +1,19 @@
 import pytest
 
+from src.items.dependencies import get_repository
 from src.items.schemas import Item, ItemCreate
+from src.main import app
 from tests.test_items.fake_repository import FakeItemRepository
 
 
 @pytest.fixture(autouse=True)
-def patch_mongo(monkeypatch) -> FakeItemRepository:
-    fake_mongo = FakeItemRepository()
-    monkeypatch.setattr("src.items.views.repository", fake_mongo)
-    return fake_mongo
+def patch_mongo() -> FakeItemRepository:
+    repository = FakeItemRepository()
+    def fake_mongo_dep() -> FakeItemRepository:
+        return repository
+
+    app.dependency_overrides[get_repository] = fake_mongo_dep
+    return repository
 
 
 @pytest.fixture
